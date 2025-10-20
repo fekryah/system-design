3af30c54d40820409f5a0c4951870814da25f0d7
fekryah <fekry.ah455@gmail.com>
2025-10-20 20:19:16 +0300

    feat(lru-cache): implement LRU cache

--- caching/lru-cache.test.ts
+++ caching/lru-cache.test.ts
@@ -1,13 +1,6 @@
 import { LeastRecentlyUsedCache } from './lru-cache';
 
-/**
- * - set and get
- * - get in-existing key
- * - set, then fill it and try to get first one
- * - set, then fill it before end, and try to get first one you pushed
- *
- *
- */
+const CACHE_CAPACITY = 10;
 
 describe('Least recently used cache test', () => {
   it('returns the stored value when the key exists', () => {
@@ -43,14 +36,13 @@ describe('Least recently used cache test', () => {
   });
 
   it('returns undefined for evicted keys', () => {
-    const cacheCapacity = 10;
-    const cache = new LeastRecentlyUsedCache<string>(cacheCapacity);
+    const cache = new LeastRecentlyUsedCache<string>(CACHE_CAPACITY);
 
     const key = 'MAIN_KEY';
     const value = 'MAIN_VALUE';
     cache.set(key, value);
 
-    for (let i = 0; i < cacheCapacity; i++) {
+    for (let i = 0; i < CACHE_CAPACITY; i++) {
       cache.set(`KEY__${i}`, i.toString());
     }
 
@@ -58,20 +50,19 @@ describe('Least recently used cache test', () => {
   });
 
   it('returns the stored value for stored, but not evicted keys', () => {
-    const cacheCapacity = 10;
-    const cache = new LeastRecentlyUsedCache<string>(cacheCapacity);
+    const cache = new LeastRecentlyUsedCache<string>(CACHE_CAPACITY);
 
     const key = 'MAIN_KEY';
     const value = 'MAIN_VALUE';
     cache.set(key, value);
 
-    for (let i = 0; i < Math.round(cacheCapacity / 2); i++) {
+    for (let i = 0; i < Math.round(CACHE_CAPACITY / 2); i++) {
       cache.set(`KEY__${i}__FIRST_ROUND`, i.toString());
     }
 
     expect(cache.get(key)).toBe(value);
 
-    for (let i = 0; i < cacheCapacity; i++) {
+    for (let i = 0; i < CACHE_CAPACITY; i++) {
       cache.set(`KEY__${i}__SECOND_ROUND`, i.toString());
     }
 
@@ -79,20 +70,19 @@ describe('Least recently used cache test', () => {
   });
 
   it('updates most recent used keys on setting key value', () => {
-    const cacheCapacity = 10;
-    const cache = new LeastRecentlyUsedCache<string>(cacheCapacity);
+    const cache = new LeastRecentlyUsedCache<string>(CACHE_CAPACITY);
 
     const key = 'MAIN_KEY';
     const value = 'MAIN_VALUE';
     cache.set(key, value);
 
-    for (let i = 0; i < Math.round(cacheCapacity / 2); i++) {
+    for (let i = 0; i < Math.round(CACHE_CAPACITY / 2); i++) {
       cache.set(`KEY__${i}__FIRST_ROUND`, i.toString());
     }
 
     cache.set(key, value);
 
-    for (let i = 0; i < cacheCapacity - 1; i++) {
+    for (let i = 0; i < CACHE_CAPACITY - 1; i++) {
       cache.set(`KEY__${i}__SECOND_ROUND`, i.toString());
     }
 
@@ -100,20 +90,19 @@ describe('Least recently used cache test', () => {
   });
 
   it('updates most recent used keys on getting key value', () => {
-    const cacheCapacity = 10;
-    const cache = new LeastRecentlyUsedCache<string>(cacheCapacity);
+    const cache = new LeastRecentlyUsedCache<string>(CACHE_CAPACITY);
 
     const key = 'MAIN_KEY';
     const value = 'MAIN_VALUE';
     cache.set(key, value);
 
-    for (let i = 0; i < Math.round(cacheCapacity / 2); i++) {
+    for (let i = 0; i < Math.round(CACHE_CAPACITY / 2); i++) {
       cache.set(`KEY__${i}__FIRST_ROUND`, i.toString());
     }
 
     cache.get(key);
 
-    for (let i = 0; i < cacheCapacity - 1; i++) {
+    for (let i = 0; i < CACHE_CAPACITY - 1; i++) {
       cache.set(`KEY__${i}__SECOND_ROUND`, i.toString());
     }
 
--- caching/lru-cache.ts
+++ caching/lru-cache.ts
@@ -1,13 +1,79 @@
 import { AbstractCache } from './abstract-cahce';
 
+class Node<T> {
+  value: T;
+  key: string;
+  next: Node<T> | undefined;
+  prev: Node<T> | undefined;
+
+  constructor(key: string, value: T) {
+    this.key = key;
+    this.value = value;
+  }
+}
+
 export class LeastRecentlyUsedCache<T> extends AbstractCache<T> {
+  private capacity: number;
+  private map: Map<string, Node<T>>;
+  private head: Node<T>;
+  private tail: Node<T>;
+
   constructor(capacity: number) {
     super();
+    this.capacity = capacity;
+    this.map = new Map();
+    this.head = new Node('UNIQUE TAIl', 's' as T);
+    this.tail = new Node('UNIQUE TAIL', 's' as T);
+
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
   }
 
   get(key: string): T | undefined {
-    return;
+    const node = this.map.get(key);
+    if (!node) return undefined;
+
+    this.moveToFront(node);
+
+    return node.value;
   }
 
-  set(key: string, value: T): void {}
+  set(key: string, value: T): void {
+    const node = this.map.get(key);
+
+    if (!node) {
+      const newNode = new Node(key, value);
+      this.addToFront(newNode);
+      this.map.set(key, newNode);
+    } else {
+      node.value = value;
+      this.moveToFront(node);
+    }
+
+    if (this.map.size > this.capacity) this.evict();
+  }
+
+  private moveToFront(node: Node<T>) {
+    this.removeNode(node);
+    this.addToFront(node);
+  }
+
+  private addToFront(node: Node<T>) {
+    node.next = this.head.next;
+    this.head.next!.prev = node;
+
+    node.prev = this.head;
+    this.head.next = node;
+  }
+
+  private removeNode(node: Node<T>) {
+    node.prev!.next = node.next;
+    node.next!.prev = node.prev;
+  }
+
+  private evict() {
+    const lru = this.tail.prev!;
+    this.removeNode(lru);
+    this.map.delete(lru.key);
+  }
 }
